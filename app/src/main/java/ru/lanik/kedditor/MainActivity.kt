b5c9bf4091cae362cccedb769a1f955780f5113b
Lanik <daniilt971@gmail.com>
2023-06-08 12:22:53 +0300

    core: Init project files

---
+++ app/src/main/java/ru/lanik/kedditor/MainActivity.kt
@@ -0,0 +1,29 @@
+package ru.lanik.kedditor
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Modifier
+import ru.lanik.kedditor.ui.theme.KedditorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KedditorTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Text(
+                        text = "Hello world!",
+                    )
+                }
+            }
+        }
+    }
+}
