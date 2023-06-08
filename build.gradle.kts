afe701553805367c25ae5bf584a5988f7937be17
Lanik <daniilt971@gmail.com>
2023-06-08 17:02:41 +0300

    gradle: Add new dependencies and plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,5 +3,10 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
+
+    // Is third party plugins
+    alias(libs.plugins.kapt) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
+    alias(libs.plugins.daggerPlugin) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
