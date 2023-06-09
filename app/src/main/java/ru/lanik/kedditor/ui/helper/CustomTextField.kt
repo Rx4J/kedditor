04b3b580ae4a7669c276dcb1287487e4c75d7a9d
Lanik <daniilt971@gmail.com>
2023-06-09 20:25:25 +0300

    src: ui: helper: Add CustomPaddingTextField function

---
+++ app/src/main/java/ru/lanik/kedditor/ui/helper/CustomTextField.kt
@@ -0,0 +1,55 @@
+package ru.lanik.kedditor.ui.helper
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CustomPaddingTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    textStyle: TextStyle = LocalTextStyle.current,
+    placeholder: @Composable (() -> Unit)? = null,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    keyboardActions: KeyboardActions = KeyboardActions(),
+    colors: TextFieldColors = TextFieldDefaults.outlinedTextFieldColors(),
+    contentPadding: PaddingValues = PaddingValues(10.dp),
+) {
+    BasicTextField(
+        value = value,
+        modifier = modifier,
+        onValueChange = onValueChange,
+        textStyle = textStyle,
+        keyboardOptions = keyboardOptions,
+        keyboardActions = keyboardActions,
+        singleLine = true,
+        decorationBox = @Composable { innerTextField ->
+            TextFieldDefaults.OutlinedTextFieldDecorationBox(
+                value = value,
+                visualTransformation = VisualTransformation.None,
+                innerTextField = innerTextField,
+                placeholder = placeholder,
+                singleLine = true,
+                enabled = true,
+                isError = false,
+                interactionSource = remember { MutableInteractionSource() },
+                colors = colors,
+                contentPadding = contentPadding,
+            )
+        },
+    )
+}
