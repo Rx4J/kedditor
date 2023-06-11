6245296e5b8c4bcf849b442239bc5ead181062d0
Lanik <daniilt971@gmail.com>
2023-06-11 16:15:31 +0300

    net: src: api: Add dto for subreddits api response

---
+++ network/src/main/java/ru/lanik/network/api/dto/subredditDTO/SubredditDto.kt
@@ -0,0 +1,30 @@
+package ru.lanik.network.api.dto.subredditDTO
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SubredditDto(
+    val data: SubredditDataDto,
+) {
+    @Serializable
+    data class SubredditDataDto(
+        @SerialName("display_name")
+        val displayName: String,
+        @SerialName("display_name_prefixed")
+        val displayNamePrefixed: String,
+        val subscribers: Int?,
+        val name: String,
+        @SerialName("icon_img")
+        val iconImg: String?,
+        @SerialName("community_icon")
+        val iconCommunity: String?,
+        val created: Double?,
+        @SerialName("user_is_subscriber")
+        val isSubscriber: Boolean?,
+        val id: String,
+        @SerialName("header_title")
+        val description: String?,
+        val url: String?,
+    )
+}
