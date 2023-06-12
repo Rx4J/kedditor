f6ef4b49705540f7df8c097e619aaae62086532e
Lanik <daniilt971@gmail.com>
2023-06-12 20:43:54 +0300

    net: src: api: Drop useless param

--- network/src/main/java/ru/lanik/network/models/Subreddit.kt
+++ network/src/main/java/ru/lanik/network/models/Subreddit.kt
@@ -4,7 +4,6 @@ data class Subreddit(
     override val id: String,
     override val name: String,
     val namePrefixed: String,
-    val nameWithoutPrefix: String,
     val url: String?,
     val imageUrl: String?,
     val isSubscriber: Boolean?,
