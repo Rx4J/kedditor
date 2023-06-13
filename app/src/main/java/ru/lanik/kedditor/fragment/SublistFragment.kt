765f808502cc8c9951cdc0e419ec2b48217de921
Lanik <daniilt971@gmail.com>
2023-06-13 15:38:23 +0300

    app: src: fragment: Inject SublistViewModel

--- app/src/main/java/ru/lanik/kedditor/fragment/SublistFragment.kt
+++ app/src/main/java/ru/lanik/kedditor/fragment/SublistFragment.kt
@@ -6,12 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import ru.lanik.kedditor.ui.screen.sublist.SublistScreen
+import ru.lanik.kedditor.ui.screen.sublist.SublistViewModel
 import ru.lanik.kedditor.ui.theme.KedditorTheme
 
 @AndroidEntryPoint
 class SublistFragment : Fragment() {
+    private val viewModel: SublistViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
