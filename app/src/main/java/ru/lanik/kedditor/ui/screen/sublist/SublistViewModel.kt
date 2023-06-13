cedfd920faedc7bbc5e275287fd0781b5011c5c3
Lanik <daniilt971@gmail.com>
2023-06-13 15:36:49 +0300

    app: src: ui: Add Sublist ViewModel

---
+++ app/src/main/java/ru/lanik/kedditor/ui/screen/sublist/SublistViewModel.kt
@@ -0,0 +1,59 @@
+package ru.lanik.kedditor.ui.screen.sublist
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import ru.lanik.kedditor.model.SublistModel
+import ru.lanik.kedditor.repository.SubredditsRepository
+import ru.lanik.network.constants.ApiSubredditSource
+import javax.inject.Inject
+
+@HiltViewModel
+class SublistViewModel @Inject constructor(
+    private val compositeDisposable: CompositeDisposable,
+    private val subredditsRepository: SubredditsRepository.Reactive,
+) : ViewModel() {
+    private val _sublistViewState: MutableStateFlow<SublistModel> by lazy {
+        val data = MutableStateFlow(SublistModel())
+        subredditsRepository.postFetchData.subscribe { newValue ->
+            data.value = newValue
+        }.also { compositeDisposable.add(it) }
+        subredditsRepository.fetchSubreddits(ApiSubredditSource.DEFAULT, false)
+        return@lazy data
+    }
+    val sublistViewState: StateFlow<SublistModel> = _sublistViewState.asStateFlow()
+
+    init {
+        if (_sublistViewState.value.subreddits == null) {
+            refreshSublist(ApiSubredditSource.DEFAULT, false)
+        }
+    }
+
+    fun onSearching(str: String) {
+        subredditsRepository.getSubredditsByName(
+            isAuth = false,
+            query = str,
+            limit = 5,
+        )
+    }
+
+    fun onBackClicked() {
+        // TODO
+    }
+
+    fun onDirectClicked(name: String) {
+        // TODO
+    }
+
+    private fun refreshSublist(source: ApiSubredditSource, isAuth: Boolean) {
+        subredditsRepository.fetchSubreddits(source, isAuth)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
+    }
+}
