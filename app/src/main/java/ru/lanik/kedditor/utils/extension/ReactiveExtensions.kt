e2cf4d35abc1a0b0c4a0d93b6f179b49ab8d79a8
Lanik <daniilt971@gmail.com>
2023-06-13 15:43:51 +0300

    app: src: ext: Simplify type call

--- app/src/main/java/ru/lanik/kedditor/utils/extension/ReactiveExtensions.kt
+++ app/src/main/java/ru/lanik/kedditor/utils/extension/ReactiveExtensions.kt
@@ -2,17 +2,18 @@ package ru.lanik.kedditor.utils.extension
 
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Single
+import ru.lanik.kedditor.utils.SchedulerPolicy
 
-fun <T : Any> Single<T>.applySchedulerPolicy(scheduler: ru.lanik.kedditor.utils.SchedulerPolicy): Single<T> {
+fun <T : Any> Single<T>.applySchedulerPolicy(scheduler: SchedulerPolicy): Single<T> {
     return this.subscribeOn(scheduler.backThread())
         .observeOn(scheduler.mainThread())
 }
 
-fun <T : Any> Observable<T>.applySchedulerPolicy(scheduler: ru.lanik.kedditor.utils.SchedulerPolicy): Observable<T> {
+fun <T : Any> Observable<T>.applySchedulerPolicy(scheduler: SchedulerPolicy): Observable<T> {
     return this.subscribeOn(scheduler.backThread())
         .observeOn(scheduler.mainThread())
 }
 
-fun <T : Any> Observable<T>.performOnBack(scheduler: ru.lanik.kedditor.utils.SchedulerPolicy): Observable<T> {
+fun <T : Any> Observable<T>.performOnBack(scheduler: SchedulerPolicy): Observable<T> {
     return this.subscribeOn(scheduler.backThread())
 }
