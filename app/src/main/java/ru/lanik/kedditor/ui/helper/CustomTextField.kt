69ec19d8c7fc157e8bc28f603b04e0b106662d37
Lanik <daniilt971@gmail.com>
2023-06-15 14:32:39 +0300

    app: src: ui: We want to change textColor in CustomPaddingTextField

--- app/src/main/java/ru/lanik/kedditor/ui/helper/CustomTextField.kt
+++ app/src/main/java/ru/lanik/kedditor/ui/helper/CustomTextField.kt
@@ -7,51 +7,83 @@ import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.LocalTextStyle
-import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.takeOrElse
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
+import ru.lanik.kedditor.ui.theme.KedditorTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CustomPaddingTextField(
     value: String,
+    placeholderValue: String,
     onValueChange: (String) -> Unit,
     modifier: Modifier = Modifier,
     readOnly: Boolean = false,
     textStyle: TextStyle = LocalTextStyle.current,
-    placeholder: @Composable (() -> Unit)? = null,
     keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
     keyboardActions: KeyboardActions = KeyboardActions(),
-    colors: TextFieldColors = TextFieldDefaults.outlinedTextFieldColors(),
+    colors: CustomTextFieldColors = CustomTextFieldColors.defaultKedditorColors(),
     contentPadding: PaddingValues = PaddingValues(10.dp),
 ) {
+    val textColor = textStyle.color.takeOrElse {
+        colors.textColor
+    }
+    val mergedTextStyle = textStyle.merge(TextStyle(color = textColor))
+
     BasicTextField(
         value = value,
         modifier = modifier,
         readOnly = readOnly,
         onValueChange = onValueChange,
-        textStyle = textStyle,
+        textStyle = mergedTextStyle,
         keyboardOptions = keyboardOptions,
         keyboardActions = keyboardActions,
         singleLine = true,
+        cursorBrush = SolidColor(colors.cursorColor),
         decorationBox = @Composable { innerTextField ->
             TextFieldDefaults.OutlinedTextFieldDecorationBox(
                 value = value,
                 visualTransformation = VisualTransformation.None,
                 innerTextField = innerTextField,
-                placeholder = placeholder,
+                placeholder = {
+                    Text(
+                        text = placeholderValue,
+                        style = KedditorTheme.typography.toolbar,
+                        color = colors.placeholderColor,
+                    )
+                },
                 singleLine = true,
                 enabled = true,
                 isError = false,
                 interactionSource = remember { MutableInteractionSource() },
-                colors = colors,
                 contentPadding = contentPadding,
             )
         },
     )
+}
+
+data class CustomTextFieldColors(
+    val textColor: Color,
+    val placeholderColor: Color,
+    val cursorColor: Color,
+) {
+    companion object {
+        @Composable
+        fun defaultKedditorColors(): CustomTextFieldColors {
+            return CustomTextFieldColors(
+                textColor = KedditorTheme.colors.primaryText,
+                placeholderColor = KedditorTheme.colors.secondaryText,
+                cursorColor = KedditorTheme.colors.tintColor,
+            )
+        }
+    }
 }
