eb137918a31ef33dfac32ed0efadd439e4136f66
Lanik <daniilt971@gmail.com>
2023-06-17 00:30:05 +0300

    net: src: model: Delete useless model

--- network/src/main/java/ru/lanik/network/models/SubredditSearch.kt
+++
@@ -1,8 +0,0 @@
-package ru.lanik.network.models
-
-data class SubredditSearch(
-    override val id: String,
-    override val name: String,
-    val numSubscribers: Int,
-    val communityIcon: String?,
-) : ListItem
