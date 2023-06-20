331b3637539f3c6d717467b5bb2b6ada9df445be
Lanik <daniilt971@gmail.com>
2023-06-20 17:01:42 +0300

    app: src: di: Add new providers

--- app/src/main/java/ru/lanik/kedditor/di/AppModule.kt
+++ app/src/main/java/ru/lanik/kedditor/di/AppModule.kt
@@ -5,11 +5,15 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.reactivex.rxjava3.disposables.CompositeDisposable
+import ru.lanik.kedditor.repository.PostRepository
+import ru.lanik.kedditor.repository.RxPostRepository
 import ru.lanik.kedditor.repository.RxSubredditsRepository
 import ru.lanik.kedditor.repository.SubredditsRepository
+import ru.lanik.kedditor.ui.screen.main.MainViewModelFactory
 import ru.lanik.kedditor.ui.screen.sublist.SublistViewModelFactory
 import ru.lanik.kedditor.utils.NetworkScheduler
 import ru.lanik.kedditor.utils.SchedulerPolicy
+import ru.lanik.network.api.PostAPI
 import ru.lanik.network.api.SubredditsAPI
 import javax.inject.Singleton
 
@@ -34,8 +38,24 @@ class AppModule {
 
     @Provides
     @Singleton
+    fun proviveRxPostRepository(
+        postAPI: PostAPI,
+        schedulerPolicy: SchedulerPolicy,
+        compositeDisposable: CompositeDisposable,
+    ): PostRepository.Reactive = RxPostRepository(postAPI, schedulerPolicy, compositeDisposable)
+
+    @Provides
+    @Singleton
     fun provideSublistViewModelFactory(
         compositeDisposable: CompositeDisposable,
         subredditsRepository: SubredditsRepository.Reactive,
     ): SublistViewModelFactory = SublistViewModelFactory(compositeDisposable, subredditsRepository)
+
+    @Provides
+    @Singleton
+    fun provideMainViewModelFactory(
+        compositeDisposable: CompositeDisposable,
+        postRepository: PostRepository.Reactive,
+        subredditsRepository: SubredditsRepository.Reactive,
+    ): MainViewModelFactory = MainViewModelFactory(compositeDisposable, postRepository, subredditsRepository)
 }
