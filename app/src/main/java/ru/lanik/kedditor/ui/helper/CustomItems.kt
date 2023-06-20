98491ca7f8db21080c349a38fcdcb70e9df2aba1
Lanik <daniilt971@gmail.com>
2023-06-20 20:26:13 +0300

    app: src: ui: Add PostViewItem helper fun

--- app/src/main/java/ru/lanik/kedditor/ui/helper/CustomItems.kt
+++ app/src/main/java/ru/lanik/kedditor/ui/helper/CustomItems.kt
@@ -6,16 +6,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.layout.ContentScale
@@ -27,6 +35,7 @@ import coil.request.ImageRequest
 import ru.lanik.kedditor.R
 import ru.lanik.kedditor.ui.theme.KedditorTheme
 import ru.lanik.kedditor.utils.extension.toFormatStr
+import ru.lanik.network.models.Post
 
 @Composable
 fun SubredditRow(
@@ -83,6 +92,128 @@ fun SubredditRow(
                     color = KedditorTheme.colors.secondaryText,
                     style = KedditorTheme.typography.caption,
                 )
+            }
+        }
+    }
+}
+
+@Composable
+fun PostViewItem(
+    post: Post,
+    onDirUp: (String) -> Unit = {},
+    onDirDown: (String) -> Unit = {},
+) {
+    val defaultIconUnit = @Composable {
+        Image(
+            bitmap = ImageBitmap.imageResource(id = R.drawable.reddit_icon),
+            contentDescription = null,
+        )
+    }
+    Surface(
+        shape = KedditorTheme.shapes.cornersStyle,
+        color = KedditorTheme.colors.secondaryBackground,
+        tonalElevation = 8.dp,
+        shadowElevation = 8.dp,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(KedditorTheme.shapes.generalPadding),
+        ) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                SubcomposeAsyncImage(
+                    model = ImageRequest.Builder(LocalContext.current)
+                        .data(null)
+                        .build(),
+                    contentDescription = null,
+                    contentScale = ContentScale.Crop,
+                    loading = {
+                        defaultIconUnit()
+                    },
+                    error = {
+                        defaultIconUnit()
+                    },
+                    modifier = Modifier
+                        .size(24.dp)
+                        .clip(CircleShape),
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = post.subredditNamePrefixed,
+                    color = KedditorTheme.colors.primaryText,
+                    style = KedditorTheme.typography.body,
+                )
+                Spacer(modifier = Modifier.width(12.dp))
+                Text(
+                    text = post.author,
+                    color = KedditorTheme.colors.secondaryText,
+                    style = KedditorTheme.typography.caption,
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = post.title,
+                color = KedditorTheme.colors.primaryText,
+                style = KedditorTheme.typography.heading,
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Text(
+                    text = "${post.numComments} comments",
+                    color = KedditorTheme.colors.secondaryText,
+                    style = KedditorTheme.typography.caption,
+                )
+                Spacer(modifier = Modifier.weight(1f))
+                Text(
+                    text = post.getDate(),
+                    color = KedditorTheme.colors.secondaryText,
+                    style = KedditorTheme.typography.caption,
+                )
+            }
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(
+                horizontalArrangement = Arrangement.Start,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth(),
+            ) {
+                IconButton(
+                    onClick = { onDirUp(post.url) },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.ArrowBack,
+                        contentDescription = null,
+                        tint = KedditorTheme.colors.tintColor,
+                        modifier = Modifier.rotate(90f),
+                    )
+                }
+                Row(
+                    horizontalArrangement = Arrangement.Center,
+                    modifier = Modifier.weight(2f)
+                ) {
+                    Text(
+                        text = post.score.toFormatStr("."),
+                        color = KedditorTheme.colors.secondaryText,
+                        style = KedditorTheme.typography.toolbar,
+                    )
+                }
+                IconButton(
+                    onClick = { onDirDown(post.url) },
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.ArrowBack,
+                        contentDescription = null,
+                        tint = KedditorTheme.colors.tintColor,
+                        modifier = Modifier.rotate(270f),
+                    )
+                }
             }
         }
     }
