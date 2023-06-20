221562ec5fe4e2c29535ee27d505278c654dff21
Lanik <daniilt971@gmail.com>
2023-06-20 22:40:04 +0300

    net: src: api: Add getSubredditInfo fun

--- network/src/main/java/ru/lanik/network/api/SubredditsAPI.kt
+++ network/src/main/java/ru/lanik/network/api/SubredditsAPI.kt
@@ -4,6 +4,7 @@ import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
+import ru.lanik.network.api.dto.subredditDTO.SubredditDto
 import ru.lanik.network.api.dto.subredditDTO.SubredditListingDto
 
 interface SubredditsAPI {
@@ -12,6 +13,12 @@ interface SubredditsAPI {
         @Path("source") source: String,
         @Query("after") page: String = "",
     ): Single<SubredditListingDto>
+
+    @GET("/r/{source}/about{auth}")
+    fun getSubredditInfo(
+        @Path("source") source: String?,
+        @Path("auth") auth: String = "",
+    ): Single<SubredditDto>
 
     @GET("/api/{command}")
     fun getSubredditsByName(
